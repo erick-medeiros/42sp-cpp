9ddbcf7911dc437e9076bc5592b1b9afa806702f
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-01-19 11:31:28 -0300

    feat(mod00/ex00): uppercase string in megaphone

--- mod00/ex00/megaphone.cpp
+++ mod00/ex00/megaphone.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "hello world" << std::endl;
+	int i;
+	int j;
+
+	if (argc <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		j = -1;
+		while (argv[i][++j])
+			argv[i][j] = std::toupper(argv[i][j]);
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
 }
