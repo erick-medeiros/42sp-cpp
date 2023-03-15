8eaec36db7bdfdb273f4c4d04a0ef54b38314d65
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-15 10:47:15 -0300

    test: check class Contact

--- mod00/ex01/tests.cpp
+++ mod00/ex01/tests.cpp
@@ -1,9 +1,37 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_NO_POSIX_SIGNALS
 
+#include "Contact.hpp"
 #include "doctest.h"
 
-TEST_CASE("test")
+TEST_CASE("saving information in class Contact")
 {
-	CHECK(1 == 1);
+	Contact ct;
+
+	ct.setFirstName("John");
+	ct.setLastName("Carmack");
+	ct.setNickname("Genius");
+	ct.setPhoneNumber("42");
+	ct.setDarkestSecret("Wolfenstein 3D");
+	CHECK_EQ(ct.getFirstName(), "John");
+	CHECK_EQ(ct.getLastName(), "Carmack");
+	CHECK_EQ(ct.getNickname(), "Genius");
+	CHECK_EQ(ct.getPhoneNumber(), "42");
+	CHECK_EQ(ct.getDarkestSecret(), "Wolfenstein 3D");
+}
+
+TEST_CASE("checking if all of the Contact class have been filled in")
+{
+	Contact ct;
+
+	ct.setFirstName("John");
+	CHECK_FALSE(ct.isCompleted());
+	ct.setLastName("Carmack");
+	CHECK_FALSE(ct.isCompleted());
+	ct.setNickname("Genius");
+	CHECK_FALSE(ct.isCompleted());
+	ct.setPhoneNumber("42");
+	CHECK_FALSE(ct.isCompleted());
+	ct.setDarkestSecret("Wolfenstein 3D");
+	CHECK(ct.isCompleted());
 }
