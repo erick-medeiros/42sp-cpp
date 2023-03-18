1c923e9cb97af88507f39bce8efa7f848b8933f9
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-18 20:12:58 -0300

    fix: address in stringPTR

--- mod01/ex02/main.cpp
+++ mod01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:42:50 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/03/16 18:53:08 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/18 18:36:03 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main()
 	std::string &stringREF = stringVAR;
 
 	std::cout << "memory address stringVAR: " << &stringVAR << std::endl;
-	std::cout << "memory address stringPTR: " << &stringPTR << std::endl;
+	std::cout << "memory address stringPTR: " << stringPTR << std::endl;
 	std::cout << "memory address stringREF: " << &stringREF << std::endl;
 	std::cout << "value of stringVAR: " << stringVAR << std::endl;
 	std::cout << "value pointed by stringPTR: " << *stringPTR << std::endl;
