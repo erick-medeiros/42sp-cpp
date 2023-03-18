36157b8029d051beabe3b0c9d56f29c6ffa5e4f1
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-18 20:12:12 -0300

    fix: max and min limit of zombies in the horde

--- mod01/ex01/main.cpp
+++ mod01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:42:50 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/03/16 18:34:51 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/18 20:09:23 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,15 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 		N = std::atoi(argv[1]);
 	if (N <= 0)
+	{
+		std::cerr << "Add at least one zombie" << std::endl;
 		return (1);
+	}
+	if (N > 1024)
+	{
+		std::cerr << "Too many zombies" << std::endl;
+		return (1);
+	}
 	horde = zombieHorde(N, "alpha");
 	i = 0;
 	while (i < N)
