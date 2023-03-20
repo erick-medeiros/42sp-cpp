6479c960f72f114cbd32cbfcffe5d3eaa524110e
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-20 19:18:06 -0300

    WIP: mod02/ex01

--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -6,11 +6,13 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 13:32:26 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/03/20 18:39:30 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:17:27 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+const int Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _value(0)
 {
@@ -28,7 +30,7 @@ Fixed &Fixed::operator=(const Fixed &copy)
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &copy)
 	{
-		this->_value = copy.toInt();
+		this->setRawBits(copy.getRawBits());
 	}
 	return *this;
 }
@@ -38,36 +40,35 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed::Fixed(const int value) : _value(value)
+Fixed::Fixed(const int value) : _value(value << _bits)
 {
 	std::cout << "Int constructor called" << std::endl;
 }
 
-Fixed::Fixed(const float value) : _value(value)
+Fixed::Fixed(const float value) : _value(value * (1 << _bits))
 {
 	std::cout << "Float constructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return _value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	_value = raw;
 }
 
 float Fixed::toFloat(void) const
 {
-	return (_value);
+	return ((float) _value / (1 << _bits));
 }
 
 int Fixed::toInt(void) const
 {
-	return (_value);
+	int i = _value >> _bits;
+	return (i);
 }
 
 std::ostream &operator<<(std::ostream &os, const Fixed &fixed)
--- mod02/ex01/Fixed.hpp
+++ mod02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 12:16:53 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/03/20 18:31:44 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:11:05 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ class Fixed
 
   private:
 	int              _value;
-	static const int _bits = 8;
+	static const int _bits;
 };
 
 std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
