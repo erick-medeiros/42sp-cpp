80ba2e6f1fad7a7f2ccac190f7d79cf14e8ab4ed
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-20 23:29:05 -0300

    refactor: remove comments

--- mod02/ex02/Fixed.cpp
+++ mod02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 23:23:31 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/03/20 23:23:34 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/20 23:27:55 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,30 @@ const int Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 }
 
 Fixed &Fixed::operator=(const Fixed &copy)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &copy)
-	{
 		this->setRawBits(copy.getRawBits());
-	}
 	return *this;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const int value) : _value(value << _bits)
 {
-	std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float value) : _value(roundf(value * (1 << _bits)))
 {
-	std::cout << "Float constructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
@@ -67,8 +59,7 @@ float Fixed::toFloat(void) const
 
 int Fixed::toInt(void) const
 {
-	int i = _value >> _bits;
-	return (i);
+	return (_value >> _bits);
 }
 
 std::ostream &operator<<(std::ostream &os, const Fixed &fixed)
