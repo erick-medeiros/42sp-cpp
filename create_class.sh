052adce7c4b5ab30b4ef44e27e438f3b1d65af41
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-04-13 13:17:58 -0300

    feat: update class template

--- create_class.sh
+++ create_class.sh
@@ -18,6 +18,7 @@ CLASS=$(echo "$ARG1" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
 CLASS_UPPER=$(echo "$CLASS" | tr '[:lower:]' '[:upper:]')
 FILE_CPP=$CLASS.cpp
 FILE_HPP=$CLASS.hpp
+FILE_TEST="$CLASS"_test.cpp
 DATE=$(date +"%Y/%m/%d %H:%M:%S")
 
 # fn_header <file>
@@ -46,12 +47,17 @@ fn_header ()
 
 fn_header $FILE_HPP > $FILE_HPP
 fn_header $FILE_CPP > $FILE_CPP
+fn_header $FILE_TEST > $FILE_TEST
 
 echo "
 #ifndef "$CLASS_UPPER"_HPP
 #define "$CLASS_UPPER"_HPP
 
 #include <iostream>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 
 class $CLASS
 {
@@ -71,26 +77,44 @@ echo "
 
 "$CLASS"::"$CLASS"(void)
 {
-	std::cout << \"$CLASS default constructor called\" << std::endl;
+	if (DEBUG)
+		std::cout << \"$CLASS default constructor called\" << std::endl;
 }
 
 $CLASS::$CLASS(const $CLASS &copy)
 {
-	std::cout << \"$CLASS copy constructor called\" << std::endl;
+	if (DEBUG)
+		std::cout << \"$CLASS copy constructor called\" << std::endl;
 	*this = copy;
 }
 
 $CLASS &$CLASS::operator=(const $CLASS &copy)
 {
-	std::cout << \"$CLASS copy assignment operator called\" << std::endl;
+	if (DEBUG)
+		std::cout << \"$CLASS copy assignment operator called\" << std::endl;
 	if (this != &copy)
 	{
-		(void) copy;
 	}
 	return *this;
 }
 
 "$CLASS"::~"$CLASS"(void)
 {
-	std::cout << \"$CLASS destructor called\" << std::endl;
+	if (DEBUG)
+		std::cout << \"$CLASS destructor called\" << std::endl;
 }" >> $FILE_CPP
+
+
+echo "
+#include \"$FILE_HPP\"
+#include \"doctest.h\"
+
+TEST_SUITE(\"Class $CLASS\")
+{
+	TEST_CASE(\"Orthodox Canonical Form\")
+	{
+		$CLASS obj1;
+		$CLASS obj2(obj1);
+		$CLASS obj3 = obj2;
+	}
+}" >> $FILE_TEST
