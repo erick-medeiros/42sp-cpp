468402bd6140ae1ba57818eef437fe3b0600b137
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-04-27 17:26:55 -0300

    feat: merge insertion sort

--- mod09/ex02/PmergeMe.cpp
+++ mod09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 09:27:23 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/04/26 18:09:52 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/04/27 17:26:18 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,24 +67,25 @@ double PmergeMe::_clockToMicroSeconds(clock_t &start, clock_t &end) const
 
 void PmergeMe::display(sort_t const &data) const
 {
+	vector_t::const_iterator it;
+
 	std::cout << "Before: ";
 	for (int i = 0; data.unsorted[i]; i++)
 		std::cout << data.unsorted[i] << " ";
 	std::cout << std::endl;
 	std::cout << "After:  ";
-	for (std::list<unum_t>::const_iterator it = data.list.begin();
-	     it != data.list.end(); it++)
+	for (it = data.vector.begin(); it != data.vector.end(); it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
 	std::cout << "Time to process a range of " << data.list.size()
 	          << " elements with std::list : " << data.timeList << " µs"
 	          << std::endl;
 	std::cout << "Time to process a range of " << data.vector.size()
-	          << " elements with std::list : " << data.timeVector << " µs"
+	          << " elements with std::vector : " << data.timeVector << " µs"
 	          << std::endl;
 }
 
-void PmergeMe::sort(char const **numbers) const
+void PmergeMe::sort(char const **numbers)
 {
 	if (!numbers)
 		return;
@@ -93,35 +94,157 @@ void PmergeMe::sort(char const **numbers) const
 	data.unsorted = numbers;
 	start = clock();
 	_fill(data.list, numbers);
+	_mergeInsertSort(data.list);
 	end = clock();
 	data.timeList = _clockToMicroSeconds(start, end);
 	start = clock();
 	_fill(data.vector, numbers);
+	_mergeInsertSort(data.vector, 0, data.vector.size());
 	end = clock();
 	data.timeVector = _clockToMicroSeconds(start, end);
 	display(data);
 }
 
-void PmergeMe::_fill(list_t &container, const char **numbers) const
+template <typename T>
+void PmergeMe::_fill(T &container, const char **numbers) const
 {
-	if (numbers)
-		for (int i = 0; numbers[i]; i++)
-			container.push_back(std::atoll(numbers[i]));
+	if (!numbers)
+		return;
+	for (int i = 0; numbers[i]; i++)
+		container.push_back(std::atoll(numbers[i]));
 }
 
-void PmergeMe::_fill(vector_t &container, const char **numbers) const
+// vector
+
+void PmergeMe::_mergeInsertSort(vector_t &container, unum_t min, unum_t max)
 {
-	if (numbers)
-		for (int i = 0; numbers[i]; i++)
-			container.push_back(std::atoll(numbers[i]));
+	if ((max - min) <= THRESHOLD)
+		_insertSort(container, min, max);
+	else
+	{
+		unum_t mid = (max + min) >> 1; // n/2
+
+		_mergeInsertSort(container, min, mid);
+		_mergeInsertSort(container, mid, max);
+
+		_mergeSort(container, min, max, mid);
+	}
 }
 
-void PmergeMe::_mergeInsert(list_t &container)
+void PmergeMe::_insertSort(vector_t &container, unum_t start, unum_t end)
 {
-	(void) container;
+	unum_t i = 0, j = 0;
+
+	for (i = start; i < end; i++)
+	{
+		unum_t it = container[i];
+		j = i;
+		while (j > start && it < container[j - 1])
+		{
+			container[j] = container[j - 1];
+			j--;
+		}
+		container[j] = it;
+	}
 }
 
-void PmergeMe::_mergeInsert(vector_t &container)
+void PmergeMe::_mergeSort(vector_t &container, unum_t min, unum_t max,
+                          unum_t mid)
 {
-	(void) container;
+	unum_t firstIndex = min;
+	unum_t secondIndex = mid + 1;
+	unum_t index;
+
+	vector_t temp(max - min + 1, 0);
+
+	for (index = min; index <= max; index++)
+	{
+		if (firstIndex <= mid &&
+		    (secondIndex > max ||
+		     container[firstIndex] <= container[secondIndex]))
+			temp[index - min] = container[firstIndex++];
+		else
+			temp[index - min] = container[secondIndex++];
+	}
+	index = 0;
+	while ((index + min) <= max)
+	{
+		container[index + min] = temp[index];
+		index++;
+	}
+}
+
+// list
+
+void PmergeMe::_mergeInsertSort(list_t &container)
+{
+	std::list<list_t> lists;
+	_divideAndConquer(container, lists);
+	while (lists.size() > 1)
+	{
+		list_t merged;
+		list_t left(lists.front());
+		lists.pop_front();
+		list_t right(lists.front());
+		lists.pop_front();
+
+		_mergeSort(left, right, merged);
+		lists.push_back(merged);
+	}
+	if (!lists.empty())
+		container = lists.front();
+}
+
+void PmergeMe::_divideAndConquer(list_t &container, std::list<list_t> lists)
+{
+	if (container.size() <= THRESHOLD)
+	{
+		lists.push_back(container);
+	}
+	else
+	{
+		list_t::iterator mid = container.begin();
+		std::advance(mid, container.size() / 2);
+
+		list_t list1;
+		list_t list2;
+		list1.splice(list1.begin(), container, container.begin(), mid);
+		list2.splice(list2.begin(), container, mid, container.end());
+
+		_divideAndConquer(list1, lists);
+		_divideAndConquer(list2, lists);
+	}
+}
+
+void PmergeMe::_insertSort(list_t &list)
+{
+	list_t::iterator i, j;
+
+	for (i = list.begin(); i != list.end(); i++)
+	{
+		unum_t curr = *i;
+		j = i;
+		while (j != list.begin() && curr < *--j)
+		{
+			unum_t prev = *j++;
+			*j-- = prev;
+		}
+		*j = curr;
+	}
+}
+
+void PmergeMe::_mergeSort(list_t &list1, list_t &list2, list_t &merged)
+{
+	list_t::iterator it1 = list1.begin();
+	list_t::iterator it2 = list2.begin();
+
+	while (it1 != list1.end() && it2 != list2.end())
+	{
+		if (*it1 <= *it2)
+			merged.splice(merged.end(), list1, it1++);
+		else
+			merged.splice(merged.end(), list2, it2++);
+	}
+	merged.splice(merged.end(), list1);
+	merged.splice(merged.end(), list2);
 }
--- mod09/ex02/PmergeMe.hpp
+++ mod09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 09:27:23 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/04/26 18:09:59 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/04/27 17:11:38 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@
 #ifndef DEBUG
 #define DEBUG 0
 #endif
+
+#define THRESHOLD 10
 
 class PmergeMe
 {
@@ -49,15 +51,19 @@ class PmergeMe
 
 	static bool checkArgs(char const **argv);
 
-	void sort(const char **numbers) const;
+	void sort(const char **numbers);
 	void display(sort_t const &data) const;
 
   private:
 	double _clockToMicroSeconds(clock_t &start, clock_t &end) const;
-	void   _fill(list_t &container, const char **numbers) const;
-	void   _fill(vector_t &container, const char **numbers) const;
-	void   _mergeInsert(list_t &container);
-	void   _mergeInsert(vector_t &container);
+	template <typename T> void _fill(T &container, const char **numbers) const;
+	void _mergeInsertSort(vector_t &container, unum_t min, unum_t max);
+	void _insertSort(vector_t &container, unum_t start, unum_t end);
+	void _mergeSort(vector_t &container, unum_t min, unum_t max, unum_t mid);
+	void _mergeInsertSort(list_t &container);
+	void _divideAndConquer(list_t &container, std::list<list_t> lists);
+	void _insertSort(list_t &container);
+	void _mergeSort(list_t &list1, list_t &list2, list_t &merged);
 };
 
 #endif /* PMERGEME_HPP */
